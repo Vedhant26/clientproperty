80d6f167b80658024ad9d48635602add69f77c7b
Vedhant26 <vedhantvarnika123@gmail.com>
2026-04-05 00:11:40 +0530

    Fix build and 404 subdirectory

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Client-Property-Seller/',
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    minify: 'terser',
     cssMinify: true,
   },
 });
