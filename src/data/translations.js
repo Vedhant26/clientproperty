c493180fe26558dc8bff2beae9999a2e0e94c27e
Vedhant26 <vedhantvarnika123@gmail.com>
2026-04-15 22:50:47 +0530

    RGDGDRG

--- src/data/translations.js
+++ src/data/translations.js
@@ -134,7 +134,7 @@ export const translations = {
     'footer.quick_links': 'Quick Links',
     'footer.contact': 'Contact Info',
     'footer.rights': '© 2024 Mahakal Property Dealer. All rights reserved.',
-    'footer.dev': 'Website developed by Vedhant Bidari - 7411176285',
+    'footer.dev': 'MP30 SHIVAM',
   },
   hi: {
     // Navbar
@@ -271,6 +271,6 @@ export const translations = {
     'footer.quick_links': 'महत्वपूर्ण लिंक्स',
     'footer.contact': 'संपर्क जानकारी',
     'footer.rights': '© 2024 महाकाल प्रॉपर्टी डीलर. सर्वाधिकार सुरक्षित.',
-    'footer.dev': 'वेबसाइट वेदांत बिदारी (Vedhant Bidari - 7411176285) द्वारा विकसित',
+    'footer.dev': 'MP30',
   }
 };
